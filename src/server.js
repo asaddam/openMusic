d4c6fa943932fd924265353320d817d53f11641b
user <user@cpe-172-100-0-52.twcny.res.rr.com>
2021-06-07 11:26:17 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/song');
+const SongsService = require('./services/postgres/songsService');
+const SongValidator = require('./validator/songs');
+require('dotenv').config();
+
+const init = async () => {
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+const mapGetAll = ({ id, title, performer }) => ({
+  id,
+  title,
+  performer,
+});
+
+const mapGetDetail = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  inserted_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  insertedAt: inserted_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapGetAll, mapGetDetail };
